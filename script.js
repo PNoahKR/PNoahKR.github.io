9c72c63575b041289ff87f7332977d127cdad105
noah <qkrwldn0818@naver.com>
2024-11-26 18:11:11 +0900

    새 배포 2

--- script.js
+++ script.js
@@ -1,34 +1,10 @@
 // 네비게이션 바 및 버튼 요소 선택
-const sidebar = document.getElementById('sidebar');
-const toggleButton = document.querySelector('.navbar-toggler');
+document.addEventListener('DOMContentLoaded', () => {
+    const sidebar = document.getElementById('sidebar');
+    const toggleButton = document.querySelector('.navbar-toggler');
 
-// 네비게이션 토글 동작
-toggleButton.addEventListener('click', () => {
-    const isMobile = window.innerWidth <= 768;
-    if (isMobile) {
+// 토글 동작
+    toggleButton.addEventListener('click', () => {
         sidebar.classList.toggle('active');
-        toggleBodyScroll(sidebar.classList.contains('active'));
-    }
-});
-
-// 스크롤 방지 함수
-function toggleBodyScroll(isSidebarActive) {
-    if (isSidebarActive) {
-        document.body.style.overflow = 'hidden';
-    } else {
-        document.body.style.overflow = '';
-    }
-}
-
-// 윈도우 크기 변경 이벤트: 사이드바 상태 초기화
-window.addEventListener('resize', () => {
-    const isMobile = window.innerWidth <= 768;
-
-    if (!isMobile) {
-        sidebar.classList.add('active');
-        document.body.style.overflow = '';
-    } else {
-        sidebar.classList.remove('active');
-        document.body.style.overflow = '';
-    }
+    });
 });
